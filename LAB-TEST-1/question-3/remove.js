a8cd397f6e7be92fb9cb41b28b3bf6c675d322bb
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2022-10-06 16:57:09 -0400

    done

--- LAB-TEST-1/question-3/remove.js
+++ LAB-TEST-1/question-3/remove.js
@@ -6,24 +6,29 @@ const dir = "./Logs";
 
 const removeLogFiles = () => {
 
-    fs.readdir(dir, (err, files) => {
-        // if folder doesnt exist or any other errors print error message 
-        if (err) console.error(`error occured with ${dir} directory`)
-        else {
-            for (let file of files) {
-                // remove files from the given directory
-                fs.unlink(path.join(dir, file), err => {
-                    // print error message if any otherwise print success message
-                    err ? console.error(err.message) : console.log(`delete files...'${file}'`)
-                });
+    if (fs.existsSync(dir)) {
+        fs.readdir(dir, (err, files) => {
+            // if any errors occure print error message 
+            if (err) console.error(`error occured with ${dir} directory`)
+            else {
+                for (let file of files) {
+                    // remove files from the given directory
+                    fs.unlink(path.join(dir, file), err => {
+                        // print error message if any otherwise print success message
+                        err ? console.error(err.message) : console.log(`delete files...'${file}'`)
+                    });
+                }
+                // remove folder itself
+                fs.rm(dir, { recursive: true, force: true }, err => {
+                    // print error if any otherwise print success message
+                    err ? console.error(err.message) : console.log(`directory ${dir} was deleted!`)
+                })
             }
-            // remove folder itself
-            fs.rm(dir, { recursive: true, force: true }, err => {
-                // print error if any otherwise print success message
-                err ? console.error(err.message) : console.log(`directory ${dir} was deleted!`)
-            })
-        }
-    });
+        });
+    }
+    else console.error(`directory ${dir} does not exists`)
+    
+    
 }
 
 // exporting function 
